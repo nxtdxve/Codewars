3358808bffb2dcdddf9032c324020b93852db118
nxtdxve <zettlerd@bzz.ch>
2022-11-10 09:14:19 +0100

    Solutions

--- JavaScript/7kyu/Fizz Buzz.js
+++ JavaScript/7kyu/Fizz Buzz.js
@@ -1,5 +1,6 @@
 // https://www.codewars.com/kata/5300901726d12b80e8000498/
 
+// solution 1
 function fizzbuzz(n)
 {
     var arr = [];
@@ -13,6 +14,17 @@ function fizzbuzz(n)
     return arr;
 }
 
+// solution 2
+function fizzbuzz(n)
+{
+    var arr = [];
+    for (var i = 1; i <= n; i++) {
+        arr.push(i % 15 == 0 ? 'FizzBuzz' : i % 3 == 0 ? 'Fizz' : i % 5 == 0 ? 'Buzz' : i);
+    }
+    return arr;
+}
+
+console.log(fizzbuzz(19));
 
 
 
