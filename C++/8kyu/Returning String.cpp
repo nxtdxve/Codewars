c9e50eb029344ff5347d6f7803d1d42450395458
nxtdxve <zettlerd@bzz.ch>
2022-11-16 15:49:09 +0100

    Solutions

---
+++ C++/8kyu/Returning String.cpp
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/55a70521798b14d4750000a4/
+
+#include <string>
+
+std::string greet(const std::string& n){
+    return "Hello, " + n + " how are you doing today?";
+}
