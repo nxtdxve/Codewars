c0b89e9e171b6e1380dafaff9789690c68360df5
nxtdxve <zettlerd@bzz.ch>
2022-11-17 08:13:37 +0100

    Solutions

--- JavaScript/7kyu/Leap Years.js
+++ JavaScript/7kyu/Leap Years.js
@@ -1,5 +1,11 @@
 // https://www.codewars.com/kata/526c7363236867513f0005ca/
 
+// Solution 1
 function isLeapYear(year) {
     return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+  }
+
+// Solution 2
+function isLeapYear(year) {
+    return new Date(year, 1, 29).getDate() === 29;
   }
