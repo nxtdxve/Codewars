ff4e6e725bbe568317fbcec6da6511606de6ea9a
nxtdxve <zettlerd@bzz.ch>
2022-11-17 09:19:02 +0100

    Solutions

--- JavaScript/6kyu/Split String.js
+++ JavaScript/6kyu/Split String.js
@@ -1,9 +1,20 @@
 // https://www.codewars.com/kata/515de9ae9dcfc28eb6000001/
 
+// Solution 1
 function solution(str){
     let arr = [];
     for (let i = 0; i < str.length; i += 2) {
       arr.push(str[i] + (str[i + 1] || '_'));
     }
     return arr;
+  }
+
+
+// Solution 2
+function solution(str){
+    let arr = [];
+    for (let i = 0; i < str.length; i += 2) {
+      arr.push(str.slice(i, i + 2));
+    }
+    return arr.map(x => x.length === 2 ? x : x + '_');
   }
