1385856a3382c5e0bf9dc29bb1e8537deb7f12e0
nxtdxve <zettlerd@bzz.ch>
2022-11-17 09:14:14 +0100

    Solutions

--- JavaScript/7kyu/Find the nth Digit of a Number.js
+++ JavaScript/7kyu/Find the nth Digit of a Number.js
@@ -1,7 +1,13 @@
 // https://www.codewars.com/kata/577b9960df78c19bca00007e/
 
+// Solution 1
 var findDigit = function(num, nth){
     if (nth <= 0) return -1;
     let digits = Math.abs(num).toString().split('').reverse();
     return digits[nth - 1] ? +digits[nth - 1] : 0;
+  }
+
+// Solution 2
+var findDigit = function(num, nth){
+    return nth > 0 ? Math.floor(Math.abs(num) / Math.pow(10, nth - 1)) % 10 : -1;
   }
