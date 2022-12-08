c2fbf0d34512edcd5f5cb416fa63a30f74cae2be
nxtdxve <zettlerd@bzz.ch>
2022-12-08 08:00:20 +0100

    Solutions

--- JavaScript/6kyu/Roman Numerals Decoder.js
+++ JavaScript/6kyu/Roman Numerals Decoder.js
@@ -24,9 +24,9 @@ function solution(roman){
 
 // Solution 2
 function solution(roman){
-    lookup = {I:1,V:5,X:10,L:50,C:100,D:500,M:1000}
-    number = 0
-    lastVal = 0
+    const lookup = {I:1,V:5,X:10,L:50,C:100,D:500,M:1000}
+    let number = 0
+    let lastVal = 0
     for (let i = roman.length - 1; i >= 0; i--) {
         val = lookup[roman[i]]
         if (val < lastVal) {
